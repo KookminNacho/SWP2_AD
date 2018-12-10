d4893b9dbadebd5ddd188aaff7f86318a48e2a40
KookminNacho <39543045+KookminNacho@users.noreply.github.com>
2018-12-10 19:39:40 +0900

    Unit test

--- AD_Project/guess.py
+++ AD_Project/guess.py
@@ -51,32 +51,53 @@ class Guess:
             else:
                 pass
 
-    def what_have_we_done(self, wordnow): # 사용자가 컴퓨터가 사용한 단어를 받아와 텍스트로 최대 3개까지 출력
+    def what_have_we_done(self, wordnow):  # 사용자가 컴퓨터가 사용한 단어를 받아와 텍스트로 최대 3개까지 출력
         self.thirdword[2], self.thirdword[1] = self.thirdword[1], self.thirdword[0]
         self.thirdword[0] = wordnow
         self.usedword.append(wordnow)
         return self.thirdword
 
-# --------------------------------작동 확인 영역----------------------------------
-
-B = Guess( )
+# --------------------------------Unit test----------------------------------
 
 
-B.test()
+if __name__ == '__main__':
+    B = Guess()
+    B.test()
+    wordlist = ['군견', '감성', '토끼', "끼룩끼룩", "안전", "암뤟"]
 
-testword = "가가린"
-print("시작 단어: ", B.game_start())
-seccscs = B.isitin(testword)
+    print("--------------------isitin()Test----------------------")
+    for i in wordlist:  # isitin 메소드 테스트
+        testword = i
+        seccscs = B.isitin(testword)
 
-if seccscs is False:
-    print("seccscs: 불가능 ㅠ")
+        if seccscs is False:
+            print(i, "단어장에서 찾을 수 없음.")
+        else:
+            print("isitin 메소드:", seccscs)
 
-else:
-    print("isitin 메소드:", testword)
+    print("------------------game_start()Test------------------")
+    for i in range(6):
+        print("시작 단어:", B.game_start())
 
+    print("-------------------botsword()Test-------------------")
+    for i in wordlist:
+        BOTTEST = B.botsword(i)
+        if BOTTEST is False:
+            print(i[-1], "으로 시작하는 단어가 없습니다")
+        elif BOTTEST is not False:
+            print(i[-1], '으로 시작하는 단어:', BOTTEST)
+    print("------------------Usedword()Test--------------------")
+    for i in range(3):
+        B.usedword.append(wordlist[i])
+    for i in wordlist:
+        if i in B.usedword:
+            print("이미 사용한 단어:", i)
+        else:
+            print("사용하지 않은 단어:", i)
 
-BOTTEST = B.botsword('군견')
-if BOTTEST is False:
-    print("단어가 없습니다")
-elif BOTTEST is not False:
-    print('견으로 시작하는 단어', BOTTEST)
+    print("-----------------what_have_we_done()------------------")
+    count = 0
+    for i in wordlist:
+        count += 1
+        word = B.what_have_we_done(i)
+        print(count, "번째 차례", word)
